77da3bdbca69603dbdad71a24d9fd6f7fb2356c1
petrlr14 <pedrodeveloper14@gmail.com>
2020-07-22 14:52:29 -0600

    working example

---
+++ controllers/user.js
@@ -0,0 +1,8 @@
+const user = require("./../models/users");
+
+exports.getAll = async (req, res, next) => {
+  const users = await user.find({}).exec();
+  return res.status(200).json({
+    users,
+  });
+};
